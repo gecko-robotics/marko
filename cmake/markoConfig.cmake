b0618a7dbd9386bd2dab211c25200939dfc1312f
walchko <walchko@users.noreply.github.com>
2019-11-17 22:13:33 -0500

    up

--- cmake/markoConfig.cmake
+++ cmake/markoConfig.cmake
@@ -1,4 +1,13 @@
 set(marko_FOUND 1)
 set(marko_VERSION "0.0.1")
 set(marko_INCLUDE_DIRS /opt/gecko/include)
-set(marko_LIBRARIES /opt/gecko/lib/libmarko.so)
+
+if(APPLE)
+    set(marko_LIBRARIES /opt/gecko/lib/libmarko.dylib)
+elseif(UNIX AND NOT APPLE)
+    set(marko_LIBRARIES /opt/gecko/lib/libmarko.so)
+endif()
+
+message(STATUS "===============================================")
+message(STATUS " Marko ${marko_VERSION}")
+message(STATUS " Exporting marko_INCLUDE_DIRS marko_LIBRARIES")
